eb4c0771ec76593b3a87022c7349577763e1d41b
BookOwl <stanleybookowl@gmail.com>
2017-01-19 16:43:09 -0500

    Add FrontEnd trait and dependency on trusty_utils

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+extern crate trusty_utils;
+use trusty_utils::*;
+
+pub trait FrontEnd {
+    fn new() -> Self;
+    fn poll_for_event(&self) -> event::Event;
+    fn render(&self, &cursor::Cursor, &str);
+    fn request_user_input(&self, &str) -> &str; 
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
